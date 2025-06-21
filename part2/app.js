8f5e11e0bc8fe084c46471c2a2b5a0897a0fa4af
KexingWang <wkx1418763841@163.com>
2025-06-21 13:35:17 +0800

    Completed user login

--- part2/app.js
+++ part2/app.js
@@ -4,6 +4,18 @@ require('dotenv').config();
 
 const app = express();
 
+const session = require('express-session');
+// Configure session middleware
+app.use(session({
+    secret: process.env.SESSION_SECRET || '3a7b21f95d8e4c0a1f6e7d3b9c5a8f0e2d1c0b7a6f8e5d2c1a0f9e7d4c3b2a1', // Strong, unique secret
+    resave: false,
+    saveUninitialized: true,
+    cookie: { 
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 1 * 60 * 60 * 1000 // 1 hours
+     }
+}));
+
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
